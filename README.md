11a71c174c8a6071f9ef9952706e2c98413a782c
slim8 <slim.khamessi@gmail.com>
2023-03-06 15:11:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-7xxwzs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-7xxwzs)
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+// Créer un nouveau composant Button dans ce même fichier
+// et l'inclure dans le composant App
+export default function App() {
+  return <h1>hello World</h1>;
+}
